b4e975ff40fc80b7686d24e70b56cde04f064d49
GIGAS <bonjin.app@gmail.com>
2026-07-29 09:48:41 +0900

    fix: restore access to portfolio sections

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ export default function App() {
 
   return (
     <div
-      className="min-h-full"
+      className="h-full min-h-full"
       dangerouslySetInnerHTML={{ __html: markup }}
     />
   )
